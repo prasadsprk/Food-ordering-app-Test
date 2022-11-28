018c800de74e987431a59eac898b1bba3c93ba44
Rajath Nair <rajath.nair@sprktechnologies.in>
2022-11-28 13:30:48 +0530

    made changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import NavBar from './Components/NavBar/NavBar'
+import Billing from './Components/Billing/Billing'
+import MapHotels from './Components/MapHotels/MapHotels';
+import HotelDetails from './Components/HotelDetails/HotelDetails';
 
 function App() {
   return (
     <div>
       <NavBar/>
+      {/* <Billing/> */}
+      <MapHotels/>
+      <HotelDetails/>
     </div>
   );
 }
